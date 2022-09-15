ebefe0c7c2ef1a72c6e956a029cf3c7ff0628ab4
tiginamaria <tiginamaria@gmail.com>
2022-09-15 21:59:36 +0200

    Fix names

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,10 @@
 fun main(args: Array<String>) {
     val cat = Cat("Barsik", 5)
-    val dog = Dog("Bobik", 3)
-    val dog2 = Dog("Tolik", 10)
-    val pets = listOf<Pet>(cat, dog, dog2)
+    val dog = Dog("Bobik", 33)
+    val puppy = Dog("Tolik", 1)
+    val pets = listOf<Pet>(cat, dog, puppy)
     for (pet in pets) {
-        println("${pet.name} ${pet.age}")
+        println("${pet.name} - ${pet.age}")
         pet.happyBirthday()
     }
 }
