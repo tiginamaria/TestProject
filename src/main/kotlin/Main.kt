d4dc9bbe25ea3fb06a7a27256a8d1e97f19e9a1d
tiginamaria <tiginamaria@gmail.com>
2022-09-16 14:59:34 +0200

    Commit it after refactoring changes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,6 @@
 fun main(args: Array<String>) {
     val cat = Cat("Barsik", 5)
-    val dog = Dog("Bobik", 33)
+    val dog = Dog("Bobik", 3)
     val puppy = Dog("Tolik", 1)
     val pets = listOf<Pet>(cat, dog, puppy)
     for (pet in pets) {
